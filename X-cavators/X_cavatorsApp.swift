bfba0a7e8db53bcf6b9ed54773131d0b17205274
Jackson Lee <jacksonlee0131@gmail.com>
2025-12-17 18:21:09 -0800

    Initial Commit

---
+++ X-cavators/X_cavatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_cavatorsApp.swift
+//  X-cavators
+//
+//  Created by Scott Lee on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct X_cavatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
